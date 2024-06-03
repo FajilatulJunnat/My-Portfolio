62bed8855cb8fd798b95c6aec40d8d62a1aa64b6
Nipa <moninipa027@gmail.com>
2024-06-03 13:42:22 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// var typed = new Typed('.animate' , {
+//     strings : [
+//         "Fajilatul Junnat Bahadur",
+//         "a Developer",
+//         "a Designer"
+//     ],
+//     typeSpeed:50,
+//     backSpeed:50,
+//     loop:true
+// });
